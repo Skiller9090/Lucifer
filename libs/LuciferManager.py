c9c7f2a47825a240646fbd3c8dc60a29315142a9
Alex Gowers <alexspam9090@gmail.com>
2020-08-17 22:03:16 +0100

    Better Command Mapping System
    Dictionary of alias's and functions with com_args passed into the function

--- libs/LuciferManager.py
+++ libs/LuciferManager.py
@@ -8,5 +8,10 @@ class LuciferManager:
         self.alternative_shells = []
         self.gui = False
         self.next_shell_id = 0
+        self.shell_recur = 0
         self.colorama = colorama
         self.termcolor = termcolor
+
+    def end(self, *args, **kwargs):
+        print("Thank you for using Lucifer, see you next time!")
+        exit(0)
