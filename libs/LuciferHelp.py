5f7130948993ea6e0455fb19436d207d37ea1e6a
Alex Gowers <alexspam9090@gmail.com>
2020-08-18 14:25:31 +0100

    Help Menu

---
+++ libs/LuciferHelp.py
@@ -0,0 +1,33 @@
+from .termcolor import colored
+
+
+help_menu = f"""{colored('Help Menu', 'green', attrs=['bold', 'underline'])}
+==========================================================================================
+{colored('help', 'blue')}                - Displays This Menu
+{colored('name', 'blue')}                - Shows name of current shell
+{colored('id', 'blue')}                  - Displays current shell's id
+{colored('show', 'blue')}                - Shows options or modules based on input,\
+ EX: show <options/modules> 
+{colored('options', 'blue')}             - Shows a list of variable/options already set
+{colored('set', 'blue')}                 - Sets a variable or option, EX: set <var> <data>
+{colored('set_vars', 'blue')}            - Auto sets need variables for loaded module
+{colored('description', 'blue')}         - Displays description of the module loaded
+{colored('auto_vars', 'blue')}           - Displays is auto_vars is True or False for\
+current shell 
+{colored('change_auto_vars', 'blue')}    - Changes the auto_var options for one shell, \
+all shells or future shells 
+{colored('use', 'blue')}                 - Move into a module, EX: use <module>
+{colored('run', 'blue')}                 - Runs the current module, can also use \
+exploit to do the same 
+{colored('spawn_shell', 'blue')}         - Spawns a alternative shell
+{colored('open_shell', 'blue')}          - Open a shell by id EX: open_shell <id>
+{colored('show_shells', 'blue')}         - Show all shell ids and attached name
+{colored('set_name', 'blue')}            - Sets current shells name EX: set_name <name>
+{colored('set_name_id', 'blue')}         - Set a shells name by id EX: set_name_id <id> \
+<name> 
+{colored('clear', 'blue')}               - Clear screen
+{colored('close', 'blue')}               - Kills current input into opened shell
+{colored('reset', 'blue')}               - Resets Everything
+{colored('exit', 'blue')}                - Exits the program, can also use quit to do \
+the same 
+"""
--- libs/LuciferShell.py
+++ libs/LuciferShell.py
@@ -1,5 +1,6 @@
 from .utils import check_int
 from .LuciferErrors import checkErrors
+from .LuciferHelp import help_menu
 import re
 import os
 import importlib
@@ -18,22 +19,7 @@ class Shell:
         self.shell_in = ""
         self.vars = {}
         self.auto_vars = lucifer_manager.auto_vars
-        self.help_menu = """help        - Displays This Menu
-show        - Shows options or modules based on input, EX: show <options/modules>
-use         - Move into a module, EX: use <module>
-set         - Sets a variable or option, EX: set <var> <data>
-run         - Runs the current module, can also use exploit to do the same
-reset       - Resets Everything
-spawn_shell - Spawns a alternative shell
-open_shell  - Open a shell by id EX: open_shell <id>
-show_shells - Show all shell ids and attached name
-set_name    - Sets current shells name EX: set_name <name>
-set_name_id - Set a shells name by id EX: set_name_id <id> <name>
-close       - Closes current shell
-clear       - Clear screen
-id          - Displays current shell's id
-exit        - Exits the program, can also use quit to do the same
-name        - Shows name of current shell"""
+        self.help_menu = help_menu
         self.name = "Shell " if not self.is_main else "Main Shell"
         self.alias = {
             "help": self.help,
