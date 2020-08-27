5d556017bc79d197a31f3d759cb65abb52d65ea9
Alex Gowers <alexspam9090@gmail.com>
2020-08-27 12:10:59 +0100

    Clearing main.py down, mild restructure

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-import lucifer.Requirements
-from lucifer.ArgParse import LuciferParser
-from lucifer.Manager import LuciferManager
+from lucifer import Requirements
+from lucifer import main
 
-luciferManager = LuciferManager()
-parser = LuciferParser(luciferManager, description="Lucifer")
 
-parser.add_argument("-l", "--log-commands", dest="logger_loc", help="Enables Command Logging To File",
-                    action="store", required=False)
-parser.add_argument("-g", "--gui", help="Enables The Gui Mode",
-                    action="store_true", required=False)
-parser.add_argument("-a", "--auto-set-vars", dest="auto_set_vars", help="Enables Auto Setting of Vars On Module Load",
-                    action="store_true", required=False)
+Requirements.check_requirements()
 
-parser.args = parser.parse_args()
-# parser.check_args()
-
-parser.check_logging()
-parser.check_autoset()
-parser.check_gui()
-
-luciferManager.end()
+main.start()
