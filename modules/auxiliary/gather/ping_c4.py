7acafb8ad4b656607107c9ef3571f4710e7f0106
Alex Gowers <alexspam9090@gmail.com>
2020-09-12 15:30:23 +0100

    LMI ping c4 update

--- modules/auxiliary/gather/ping_c4.py
+++ modules/auxiliary/gather/ping_c4.py
@@ -3,22 +3,7 @@ import platform
 
 from lucifer.Errors import ArgumentUndefinedError
 from modules.Module import BaseModule
-
-
-def ping_response(host):
-    if "://" in host:
-        host = host.split("://")[1]
-
-    if platform.system() == "Windows":
-        command = "ping"
-    else:
-        command = "ping -c 4"
-
-    response = os.system(f'{command} {host}')
-    if response == 0:
-        return True
-    else:
-        return False
+from LMI import Command
 
 
 class Module(BaseModule):
@@ -27,10 +12,8 @@ class Module(BaseModule):
             hostname = self.shell.vars["hostname"]
         else:
             raise ArgumentUndefinedError("hostname")
-        if self.isShellRun:
-            print(ping_response(hostname))
-        else:
-            return ping_response(hostname)
+        response = self.ping_response(hostname)
+        return response
 
     def set_vars(self):
         new_vars = {
@@ -41,3 +24,18 @@ class Module(BaseModule):
     def get_description(self):
         desc = """Ping a host to check if it is up"""
         return desc
+
+    def ping_response(self, host):
+        if "://" in host:
+            host = host.split("://")[1]
+
+        if platform.system() == "Windows":
+            command = "ping"
+        else:
+            command = "ping -c 4"
+
+        if self.isShellRun:
+            response = Command.tee_output([command, host])
+        else:
+            response = Command.return_output([command, host])
+        return response
