9c79995e7b923a2c1242e384c20a6d1d1f35f0bc
Alex Gowers <alexspam9090@gmail.com>
2020-10-25 18:11:28 +0000

    Fixed attribute error on importing corrupt modules, added comments

--- modules/Module.py
+++ modules/Module.py
@@ -1,22 +1,26 @@
 from lucifer.Utils import RetrieveShell
 
 
-class BaseModule(RetrieveShell):
-    def __init__(self, luciferManager, ShellRun=False):
+class BaseModule(RetrieveShell):   # All Modules should inherit from BaseModule (this class)
+    def __init__(self, luciferManager, ShellRun=False):  # Module setup, DO NOT EDIT, hook onto the 'setup' instead
         self.luciferManager = luciferManager
         self.actions = []
         self.isShellRun = ShellRun
         self.get_shell()
+        self.setup()
 
-    def run(self):
+    def setup(self):  # This is a setup hook which will be run on module load
+        pass
+
+    def run(self):  # This is a run hook and will be run on the module 'run'
         for action in self.actions:
             action()
 
-    def set_vars(self):
+    def set_vars(self):  # This is set_vars hook and will return the vars to set when the command 'set_vars' is run
         new_vars = {
         }
         return new_vars
 
-    def get_description(self):
+    def get_description(self):   # This is the description hook, returns a description on command 'description'
         desc = """This Module Has No Description!"""
         return desc
