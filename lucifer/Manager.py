15de2f754d3a0f317cc2ec8c4cac168fb3c633b6
Alex Gowers <alexspam9090@gmail.com>
2021-02-20 05:28:50 +0000

    Version ++

--- lucifer/Manager.py
+++ lucifer/Manager.py
@@ -28,7 +28,7 @@ class LuciferManager:
         self.stderr = sys.stderr
         self.isLMI = False
         # MAJOR.MINOR.PATCH.STAGE.BUILD VERSIONING
-        self.numeric_version = (0, 6, 0, 3, 7)
+        self.numeric_version = (0, 6, 1, 3, 1)
         self.numeric_stage = self.numeric_version[3]
         self.stage = ["Alpha", "Beta", "RC", "Release"][self.numeric_version[3]]
         self.shortStage = ["a", "b", "rc", "r"][self.numeric_version[3]]
