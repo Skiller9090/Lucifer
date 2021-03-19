6fe76e44f79b23e68bb9e248843ea50e92af3e3d
Alex Gowers <alexspam9090@gmail.com>
2021-03-19 22:09:05 +0000

    Added __all__ vars to modules and import cleanup

--- LMI/Java/__init__.py
+++ LMI/Java/__init__.py
@@ -2,3 +2,5 @@ from . import Installs
 from . import Run
 from . import Utils
 from .Data import _JavaInstallsData
+
+__all__ = ["Data", "Utils", "Run", "Installs"]
--- LMI/Reports/__init__.py
+++ LMI/Reports/__init__.py
@@ -1,4 +1,7 @@
 from .HTML import HTMLReport as _HTMLReport
 
+
+__all__ = ["HTML"]
+
 isReporting = False
 HTMLReport = _HTMLReport()
