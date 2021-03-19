3880754ca8ca5dfb5e813019948670f8812002de
Alex Gowers <alexspam9090@gmail.com>
2021-03-19 22:09:05 +0000

    Added __all__ vars to modules and import cleanup

--- LMI/__init__.py
+++ LMI/__init__.py
@@ -3,9 +3,19 @@ LMI (Lucifer Module Interface) allows you to easily interface with different par
 
 Modules:
     - Command: Allows you to run system commands with varying levels of output
+    - Java: //
+    - Networking: //
+    - Reports: //
+    - File: //
+    - Interface: //
+    - OS: //
+    - Table: //
+    - Utils: //
 """
-from . import Java
-from . import Utils
+from . import Java, Networking, Reports, Command, File, Interface, OS, Table, Utils
 from .Interface import LuciferManagerInterface
+
+
+__all__ = ["Java", "Networking", "Reports", "Command", "File", "Interface", "OS", "Table", "Utils", "LMI"]
 
 LMI = LuciferManagerInterface.instance  # Late Definition
